85893018afe1fa4913bc76d04ad14808e5d266bf
MrCode200 <100614430+MrCode200@users.noreply.github.com>
2024-09-09 07:32:24 +0000

    Added Different varients of items in itemitemTemplate

--- app/items/itemTemplate.py
+++ app/items/itemTemplate.py
@@ -6,10 +6,34 @@ class Item(ABC):
     id_generator = idGenerator.sequential_item_id_generator()
 
     @abstractmethod
-    def __init__(self, item_type :str = "item"):
+    def __init__(self, pos: list[int]):
         self._id = next(Item.id_generator)
-        self.item_type = item_type
+        self.pos = pos
 
     @property
     def id(self):
         return self._id
+    
+
+class ConsumableItem(Item):
+    def __init__(self, pos: list[int]):
+        super().__init__(pos)
+
+    @abstractmethod 
+    def on_use(self):
+        pass
+
+
+class CraftableItem(Item):
+    def __init__(self, pos: list[int]):
+        super().__init__(pos)
+
+
+class Tool(Item):
+    def __init__(self, pos: list[int]):
+        super().__init__(pos)
+
+    @abstractmethod 
+    def on_use(self):
+        pass
+    
